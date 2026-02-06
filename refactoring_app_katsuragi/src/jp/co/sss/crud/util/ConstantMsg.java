29f478b43020eca8e8ee4edcbf54e5dfaed5709e
yuya-katsuragi <yuya_katsuragi@asupage.co.jp>
2026-02-06 10:42:17 +0900

    add:葛城 ConstanMsg.java マジックナンバー、ハードコーディングを定数化
    狙い：可読性、保守性の向上
    部署名、性別、空文字などを定数化しconstansクラスに移行しました
    命名規則 MENU_ OR MSG
    空文字や日付に関してはFORMAT_DATEなど適したフィールド名に変更しています

--- refactoring_app_katsuragi/src/jp/co/sss/crud/util/ConstantMsg.java
+++ refactoring_app_katsuragi/src/jp/co/sss/crud/util/ConstantMsg.java
@@ -3,7 +3,7 @@ package jp.co.sss.crud.util;
 public class ConstantMsg {
 
 	//メニュー番号
-
+	public static final int MENU_TITLE = 0;
 	public static final int MENU_ALL_SEARCH = 1;
 	public static final int MENU_EMPID_SEARCH = 2;
 	public static final int MENU_DEPTID_SEARCH = 3;
@@ -22,7 +22,7 @@ public class ConstantMsg {
 	public static final int DEPT_GENERAL_AFFAIRS = 3;
 
 	// メニューの表示
-	public static final String MENU_TITLE = "=== 社員管理システム ===";
+	public static final String MENU_BY_TITLE = "=== 社員管理システム ===";
 	public static final String MENU_ALL = "1.全件表示";
 	public static final String MENU_BY_EMPID = "2.社員名検索";
 	public static final String MENU_BY_DEPTID = "3.部署ID検索";
@@ -40,8 +40,8 @@ public class ConstantMsg {
 
 	public static final String MSG_DELETE_COMPLETE = "社員情報を削除しました";
 
+	//新規登録時メッセージ
 	public static final String GUIDE_EMPID = "社員名：";
-
 	public static final String GUIDE_GENDER = "性別(0:回答なし, 1:男性, 2:女性, 9:その他):";
 	public static final String GUIDE_BIRTHDAY = "生年月日(西暦年/月/日):";
 	public static final String GUIDE_DEPTID = "部署ID(1:営業部、2:経理部、3:総務部):";
@@ -51,6 +51,24 @@ public class ConstantMsg {
 	public static final String MSG_DELETE_DEPTID = "削除する社員の社員IDを入力してください：";
 
 	public static final String MSG_SYSTEM_EXIT = "システムを終了します。";
+
+	public static final String MSG_INFO_LIST = "社員ID\t社員名\t性別\t生年月日\t部署名";
+	//空文字スペース用
+	public static final String HORIZONTAL_TAB = "\t";
+	public static final String EMPTY_LINE = "";
+
+	//性別選択メッセージ
+	public static final String MSG_NO_ANSWER = "回答なし";
+	public static final String MSG_MALE = "男性";
+	public static final String MSG_FAMALE = "女性";
+	public static final String MSG_OTHER = "その他";
+
+	//部署名メッセージ
+	public static final String MSG_DEPT_SALES = "営業部";
+	public static final String MSG_DEPT_ACCOUNTING = "経理部";
+	public static final String MSG_DEPT_GENERAL_AFFAIRS = "総務部";
+
+	public static final String FORMAT_DATE = "yyyy/MM/dd";
 
 	{
 
