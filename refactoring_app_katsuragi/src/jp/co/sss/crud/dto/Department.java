b5199e73d588eb5d9f9cdd98b2d5954628886b69
yuya-katsuragi <yuya_katsuragi@asupage.co.jp>
2026-02-09 09:11:31 +0900

    add: Department.java DBからの情報を安全に使用するため
    狙い:保守性の向上、可読性の向上
    DBから受け取ってきた生のデータを扱いやすい形に変換しました。

--- refactoring_app_katsuragi/src/jp/co/sss/crud/dto/Department.java
+++ refactoring_app_katsuragi/src/jp/co/sss/crud/dto/Department.java
@@ -2,4 +2,54 @@ package jp.co.sss.crud.dto;
 
 public class Department {
 
+	private Integer deptId;
+
+	private String deptName;
+
+	public Department() {
+	}
+
+	/**
+	 * @param deptId
+	 * @param deptName
+	 */
+	public Department(Integer deptId, String deptName) {
+		super();
+		this.deptId = deptId;
+		this.deptName = deptName;
+	}
+
+	/**
+	 * @return deptId
+	 */
+	public Integer getDeptId() {
+		return deptId;
+	}
+
+	/**
+	 * @param deptId セットする deptId
+	 */
+	public void setDeptId(Integer deptId) {
+		this.deptId = deptId;
+	}
+
+	/**
+	 * @return deptName
+	 */
+	public String getDeptName() {
+		return deptName;
+	}
+
+	/**
+	 * @param deptName セットする deptName
+	 */
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [deptId=" + deptId + ", deptName=" + deptName + "]";
+	}
+
 }
